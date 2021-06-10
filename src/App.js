1623222bb988023655e30e974f0ee0bde96b7b91
Tamojay Dey <41836229+tamojay@users.noreply.github.com>
2021-06-11 00:48:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Todolist from './component/Todolist';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <Todolist/>
+    </div>
+  );
+}
+
+export default App;
