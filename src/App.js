dcddfaf3934a7ba5526b12cdca0191e1f63af6e3
Tamojay Dey <41836229+tamojay@users.noreply.github.com>
2021-06-11 10:22:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Todolist from './component/Todolist';
 
-function App() {
-  return (
-    <div className="todo-app">
-      <Todolist/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="todo-app">
+        <Todolist/>
+      </div>
+    );
+  }  
 }
 
 export default App;
+
